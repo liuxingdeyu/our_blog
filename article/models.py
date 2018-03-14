160eae90c444a6c5b2d4a0deb68c380b2f7c652a
ZhangYu <liuxingdeyu@foxmail.com>
2018-03-14 16:36:21 +0800

    Initial commit

---
+++ article/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db import connections
+# Create your models here.
+
+
+class Article(object):
+    pass
+
+
+class AddArticle(object):
+    pass
+
+
+class ArticleManager(object):
+    pass
+
+
+class GetArticleList(object):
+    pass
+
+
+class GetArticle(object):
+    pass
+
+
